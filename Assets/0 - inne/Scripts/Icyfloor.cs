e9ed08ee4f5e6d3bddde63be2610858fade6ca4f
Max2137 <maximuton2006@gmail.com>
2022-11-13 21:20:12 +0100

    enemy

--- Assets/0 - inne/Scripts/Icyfloor.cs
+++ Assets/0 - inne/Scripts/Icyfloor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyFollowPlayer : MonoBehaviour
+public class Icyfloor : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
