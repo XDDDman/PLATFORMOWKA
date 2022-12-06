aa0b8f88344a27cc109968fdeb55ac024038c673
LAB1-Student02 <maximuton2006@gmail.com>
2022-12-06 12:37:48 +0100

    raycast Mateussza

--- Assets/raycast2.cs
+++ Assets/raycast2.cs
@@ -7,7 +7,9 @@ public class raycast2 : MonoBehaviour
     int moveDirection = 1;
 
     Rigidbody2D rb2D;
-
+    bool left, right;
+    [SerializeField] bool ShowRaycast;
+    [SerializeField] LayerMask Ground;
 
     void Start()
     {
@@ -16,16 +18,51 @@ public class raycast2 : MonoBehaviour
 
     void Update()
     {
+        
         // Cast a ray straight down.
-        RaycastHit2D hit = Physics2D.Raycast(transform.position+ Vector3.right*moveDirection, -Vector2.up);
-        Debug.Log(hit.collider.gameObject.name);
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position+ Vector3.right*moveDirection, -Vector2.up);
+        //Debug.Log(hit.collider.gameObject.name);
+
+        if(ShowRaycast)
+        {
+            Debug.DrawRay(transform.position + Vector3.right * moveDirection, -transform.up * 2);
+            Debug.DrawRay(transform.position + Vector3.left * moveDirection, -transform.up * 2);
+        }
+
+        right = Physics2D.Raycast(transform.position + Vector3.right * moveDirection, -transform.up, 2f, Ground);
+         left  = Physics2D.Raycast(transform.position + Vector3.left * moveDirection, -transform.up, 2f, Ground);
 
         // If it hits something...
-        if (hit.collider == null)
+        //if (hit.collider == null)
+       // {
+         //   moveDirection *= -1;
+       // }
+
+        if(!right)
         {
-            moveDirection *= -1;
+            if(moveDirection == 1)
+            {
+                moveDirection = -1;
+            } else
+            {
+                moveDirection = 1;
+            }
+        }
+
+        if(!left)
+        {
+            if (moveDirection == 1)
+            {
+                moveDirection = -1;
+            }
+            else
+            {
+                moveDirection = 1;
+            }
         }
 
         transform.Translate(new Vector3(moveDirection * speed*Time.deltaTime, 0, 0));
     }
 }
+
+// GRUBY TU BYL
