a64f2fe1a33e8c0de55c68036a5a506df64bba2b
LAB1-Student02 <maximuton2006@gmail.com>
2022-12-13 12:42:20 +0100

    jupi

--- Assets/0 - inne/Scripts/Pause.cs
+++ Assets/0 - inne/Scripts/Pause.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    public bool pause = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+       
+       
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+    {
+        if (pause == false)
+        {
+        Time.timeScale = 0;
+        pause = true;
+        }
+        else
+        {
+        Time.timeScale = 1;
+        pause = false;
+        }
+    }
+   
+
+
+
     }
 }
+
+
+
