db12c75806abc5db2ba1d71438fa9f0a2d26931d
FranekW <franciszek.wieczorkiewicz@gmail.com>
2023-01-14 15:27:47 +0100

    asfewd

--- Assets/0 - inne/Scripts/Player_Death.cs
+++ Assets/0 - inne/Scripts/Player_Death.cs
@@ -1,12 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
+
 using UnityEngine.SceneManagement;
+
+using UnityEngine.UI;
 
 public class Player_Death : MonoBehaviour
 {
+
+    public GameObject TeloportGoal;
+
+    public GameObject Gracz;
+
+
+    public Text scoreText;
+    public int levelDeaths = 0;
+
+    void Start()
+    {
+        scoreText.text = levelDeaths.ToString();
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
-    if(col.CompareTag("Instant death"))
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        
+        if (col.CompareTag("Instant death"))
+        {
+            Gracz.transform.position = TeloportGoal.transform.position;
+
+            levelDeaths += 1;
+            scoreText.text = levelDeaths.ToString();
+        }
     }
 }
